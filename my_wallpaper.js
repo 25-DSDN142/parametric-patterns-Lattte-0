8001e482f0d97981722eaddb016d75d4f5b78495
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-07-29 10:24:16 +1200

    added PBA1 Bear

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -17,10 +17,39 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(242, 255, 240); //light honeydew green colour
+  background(242, 55, 40); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  ellipse(40 ,40, rect_width, rect_height);
+drawBear()
+}
+
+
+
+function drawBear() {
+
+ fill(206, 149, 119)
+stroke(191, 130, 101)
+circle(70,75, 40,40)  //ear
+circle(130,75, 40,40)// ear
+
+fill(191, 130, 101)
+stroke(191, 130, 101)
+circle(70,73, 20,20)  //inner ear
+circle(130,73, 20,20) //inner ear
+
+ fill(206, 149, 119)
+  ellipse(100, 110, 100, 80) //face
+
+  fill(0)
+  ellipse(75, 112, 10, 20) // eyes
+   ellipse(125, 112, 10, 20)
+   ellipse(100, 125, 10,10)  //mouth
+
+    fill(255, 190, 188)
+    stroke(255, 190, 188)
+   ellipse(67,126, 18, 10)     //cheeks
+   ellipse(133,126, 18, 10)
 
 }
