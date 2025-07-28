a6c984206b05185d6fb1a23e155ca76fe5c11201
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-07-29 11:21:52 +1200

    1-3 iterations

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,14 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
+let whiteCol = [255,255,255];
+let brownCol = [206, 149, 119];
+
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -21,12 +24,46 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  ellipse(40 ,40, rect_width, rect_height);
-drawBear()
+scale(0.5,0.5)
+  drawBears(whiteCol)
+scale(0.7,0.7)
+  drawBears(whiteCol,200,200)
+  drawBears(brownCol) 
+
+
+  
 }
 
 
 
+function drawBears(colSetting,xPos=0,yPos=0) {
+
+translate(xPos,yPos);
+fill(colSetting);
+ //fill(255,240,219)
+stroke(225,203,195)
+circle(70,75, 40,40)  //ear
+circle(130,75, 40,40)// ear
+
+//fill(238,217,196)
+stroke(225,203,195)
+circle(70,73, 20,20)  //inner ear
+circle(130,73, 20,20) //inner ear
+
+ //fill(255,240,219)
+  ellipse(100, 110, 100, 80) //face
+
+  fill(0)
+  ellipse(75, 112, 10, 20) // eyes
+   ellipse(125, 112, 10, 20)
+   ellipse(100, 125, 10,10)  //mouth
+
+    fill(255, 190, 188)
+    stroke(255, 190, 188)
+   ellipse(67,126, 18, 10)     //cheeks
+   ellipse(133,126, 18, 10)
+translate(-xPos,-yPos);
+}
 function drawBear() {
 
  fill(206, 149, 119)
