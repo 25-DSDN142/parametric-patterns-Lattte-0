8af343cccb604509995220e58ec3bc12b04a9c13
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-07-29 10:16:02 +1200

    j

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -17,7 +17,7 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(242, 255, 240); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
