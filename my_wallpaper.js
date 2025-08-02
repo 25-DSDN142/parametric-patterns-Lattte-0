c952e485efb1b5b34cc31e0c2cb0e1191d2b6824
Lattte-0 <clarkchar3@myvuw.ac.nz>
2025-08-02 17:30:28 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,18 +1,17 @@
 //your parameter variables go here!
-
-let whiteCol = [255,255,255];
-let brownCol = [206, 149, 119];
-let blackbear = [119, 73, 54]
-let bBears = (0.7,0.7)
-let wBear  = (1,1)
-
+let brownB = 100;
+let bbY  =-25;
+let blackB =-100;
+let blbY =-25;
+let whiteB =0;
+let wBY   =45;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+//pWallpaper.output_mode(DEVELOP_GLYPH);
+pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -21,43 +20,111 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(242, 55, 40); //light honeydew green colour
+  background(168, 74, 50); //A paler burnt umber colour,  168, 74, 50
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-//scale(wBear,0.5,0.5)
-  drawBears(whiteCol,wBear,34,20)
-//scale(bBears,0.7,0.7)
-  drawBears(brownCol) 
-  drawBear (blackbear,140,140)
+scale(1,1)
+  drawBearw(whiteB,wBY)
 
-if(wBear<25,15) {
+//scale(0.5,0.5)
+drawBears(brownB,bbY) 
 
-whiteCol=[206, 149, 119];
+//scale(0.5,0.5)
+drawBear(blackB,blbY)
 
-} 
-    
 
-  
 }
 
-
-
-function drawBears(colSetting,xPos=0,yPos=0) {
+function drawBears(xPos=0,yPos=0) {   //draws the brownbear, colSetting, //fill(colSetting);
 
 translate(xPos,yPos);
-fill(colSetting);
- //fill(255,240,219)
+
+//scale(0.5,0.5)
+fill(206, 149, 119)
+stroke(225,203,195)
+circle(70,75, 40,40,brownB)  //ear
+circle(130,75, 40,40,brownB)// ear
+
+fill(191, 130, 101)
+stroke(225,203,195)
+circle(70,73, 20,20,brownB)  //inner ear
+circle(130,73, 20,20,brownB) //inner ear
+
+ fill(206, 149, 119)
+  ellipse(100, 110, 100, 80,brownB) //face
+
+  fill(0)
+  ellipse(75, 112, 10, 20,brownB) // eyes
+   ellipse(125, 112, 10, 20,brownB)
+   ellipse(100, 125, 10,10,brownB)  //mouth
+
+    fill(255, 190, 188)
+    stroke(255, 190, 188,brownB)
+   ellipse(67,126, 18, 10,brownB)     //cheeks
+   ellipse(133,126, 18, 10,brownB)
+translate(-xPos,-yPos);
+}
+function drawBear(xPos=0,yPos=0) {  //draws the black bear, colSetting
+
+translate(xPos,yPos);
+//fill(colSetting);
+
+      
+stroke(47, 14, 7)
+
+let whiteCol = false
+
+fill(119, 73, 54) //normal colour
+
+if(whiteCol){
+
+  fill(242, 223, 206);  //white bear colours
+}
+
+circle(70,75, 40,40)  //ear
+circle(130,75, 40,40)// ear
+
+fill(128, 77, 32)
+stroke(101, 58, 42)
+circle(70,73, 20,20)  //inner ear
+circle(130,73, 20,20) //inner ear
+
+fill(119, 73, 54)
+
+
+if(whiteCol){
+
+  fill(242, 223, 206);  //white bear colours
+}
+  ellipse(100, 110, 100, 80) //face
+
+  fill(0)
+  ellipse(75, 112, 10, 20) // eyes
+   ellipse(125, 112, 10, 20)
+   ellipse(100, 125, 10,10)  //mouth
+
+    fill(255, 190, 188)
+    stroke(255, 190, 188)
+   ellipse(67,126, 18, 10)     //cheeks
+   ellipse(133,126, 18, 10)
+
+}
+function drawBearw(xPos=0,yPos=0) {   //draws the polar bear, colSetting
+
+translate(xPos,yPos);
+//fill(colSetting);
+ fill(255,240,219)
 stroke(225,203,195)
 circle(70,75, 40,40)  //ear
 circle(130,75, 40,40)// ear
 
-//fill(238,217,196)
+fill(238,217,196)
 stroke(225,203,195)
 circle(70,73, 20,20)  //inner ear
 circle(130,73, 20,20) //inner ear
 
- //fill(255,240,219)
+ fill(255,240,219)
   ellipse(100, 110, 100, 80) //face
 
   fill(0)
@@ -70,33 +137,4 @@ circle(130,73, 20,20) //inner ear
    ellipse(67,126, 18, 10)     //cheeks
    ellipse(133,126, 18, 10)
 translate(-xPos,-yPos);
-}
-function drawBear(colSetting,xPos=0,yPos=0) {
-
-translate(xPos,yPos);
-fill(colSetting);
-
- //fill(206, 149, 119)
-stroke(47, 14, 7)
-circle(70,75, 40,40)  //ear
-circle(130,75, 40,40)// ear
-
-//fill(191, 130, 101)
-stroke(101, 58, 42)
-circle(70,73, 20,20)  //inner ear
-circle(130,73, 20,20) //inner ear
-
- //fill(206, 149, 119)
-  ellipse(100, 110, 100, 80) //face
-
-  fill(0)
-  ellipse(75, 112, 10, 20) // eyes
-   ellipse(125, 112, 10, 20)
-   ellipse(100, 125, 10,10)  //mouth
-
-    fill(255, 190, 188)
-    stroke(255, 190, 188)
-   ellipse(67,126, 18, 10)     //cheeks
-   ellipse(133,126, 18, 10)
-
 }
